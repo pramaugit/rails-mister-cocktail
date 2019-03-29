c45c5980d731507f4128efebde6940039080023d
Praveen <maurya.praveen@gmail.com>
2019-03-29 17:47:31 +0800

    push to git origin master

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
-  belongs_to :ingredients
+  belongs_to :ingredient
   validates :description, presence: true, allow_blank: false
+  validates :ingredient, uniqueness: { scope: :cocktail}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Dose.delete_all if Rails.env.development?
+Ingredient.delete_all if Rails.env.development?
+Cocktail.delete_all if Rails.env.development?
+
+url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json"
+
+
+cocktails_array = JSON.parse(open(url).read)
+
+
+cocktails_array.each do |cocktail|
+  p cocktail
+  c = Cocktail.create!(name: cocktail["name"])
+  p "Added #{c.name} to the list"
+  cocktail["ingredients"].each do |ing|
+    unless ing["ingredient"].nil?
+      i = Ingredient.find_or_create_by!(name: ing["ingredient"])
+      p "Added #{i.name} to the list"
+      Dose.create(description: ing["amount"].to_s + " " + ing["unit"], ingredient: i, cocktail: c )
+    end
+  end
+end

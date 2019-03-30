8c80745923975d68381bd1c8fd4554c856adf16c
Praveen <maurya.praveen@gmail.com>
2019-03-30 10:46:05 +0800

    fixed photouploader bug by bundle install

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,24 @@ Dose.delete_all if Rails.env.development?
 Ingredient.delete_all if Rails.env.development?
 Cocktail.delete_all if Rails.env.development?
 
-url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json"
-
+# url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json"
 
 cocktails_array = JSON.parse(open(url).read)
 
+photos = [
+  "https://images.pexels.com/photos/312080/pexels-photo-312080.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/1232152/pexels-photo-1232152.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/1265910/pexels-photo-1265910.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+]
 
-cocktails_array.each do |cocktail|
-  p cocktail
-  c = Cocktail.create!(name: cocktail["name"])
+counter = 0
+
+cocktails_array.first(3).each do |cocktail|
+  url = photos[counter]
+  c = Cocktail.new(name: cocktail["name"])
+  c.remote_photo_url = url
   p "Added #{c.name} to the list"
+  c.save
   cocktail["ingredients"].each do |ing|
     unless ing["ingredient"].nil?
       i = Ingredient.find_or_create_by!(name: ing["ingredient"])
@@ -28,4 +36,5 @@ cocktails_array.each do |cocktail|
       Dose.create(description: ing["amount"].to_s + " " + ing["unit"], ingredient: i, cocktail: c )
     end
   end
+  counter += 1
 end
